70509fabf6b89a0fbe9d8f1aa464107fc10ca646
Joshika-jaya <joshikajehan@gmail.com>
2022-02-28 05:04:39 +0000

    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

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -25,7 +26,11 @@ import {HttpClientModule} from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent,LoginComponent,SignupComponent,AddonsComponent,PopularplansComponent]
+})
 export class AppModule { }
