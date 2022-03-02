99499741c67384a9f3076384d0e94bafae988066
sahithi23-mm <sahithimaddali23@gmail.com>
2022-03-02 11:11:26 +0000

    pulled work

--- angularapp/src/app/signup/signup.component.ts
+++ angularapp/src/app/signup/signup.component.ts
@@ -44,7 +44,7 @@ export class SignupComponent implements OnInit {
     return this.signupform.controls['confirmpassword'];
   }
   signupform=new FormGroup({
-	  email:new FormControl('',[Validators.required,Validators.email]),
+	  emailId:new FormControl('',[Validators.required,Validators.email]),
 	  username:new FormControl('',[Validators.required]),
     mobilenumber:new FormControl('',[Validators.required,Validators.pattern("[0-9 ]{10}")]),
     password:new FormControl('',[Validators.required,]),
@@ -52,7 +52,7 @@ export class SignupComponent implements OnInit {
 
   })
 
-  get email(){
+  get emailId(){
 	  return this.signupform.get('email');
   }
   get username(){
