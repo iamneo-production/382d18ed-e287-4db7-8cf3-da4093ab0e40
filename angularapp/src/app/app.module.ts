9b92c2b85427191e132ca0bd97afd162476e01b2
rishikant3 <rishikant5sep@gmail.com>
2022-03-03 07:05:44 +0000

    hi

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -9,34 +10,30 @@ import { SignupComponent } from './signup/signup.component';
 import { AddonsComponent } from './addons/addons.component';
 import { PopularplansComponent } from './popularplans/popularplans.component';
 import { DisplayUserComponent } from './display-user/display-user.component';
-import  { EditUserComponent } from './edit-user/edit-user.component';
-
+import { EditUserComponent } from './edit-user/edit-user.component';
 import {HttpClientModule} from '@angular/common/http';
-import { CommonModule } from '@angular/common';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { PrepaidplansComponent } from './prepaidplans/prepaidplans.component';
-import { RouterModule } from '@angular/router';
-
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
     AddonsComponent,
-    EditUserComponent,
     PopularplansComponent,
     DisplayUserComponent,
+    EditUserComponent,
+    AdminDashboardComponent,
     PrepaidplansComponent
-    
-   
-
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    CommonModule,
-    RouterModule
-  ],
+    HttpClientModule
 
+  ],
   providers: [],
   bootstrap: [AppComponent,LoginComponent,SignupComponent,AddonsComponent,PopularplansComponent]
 })
