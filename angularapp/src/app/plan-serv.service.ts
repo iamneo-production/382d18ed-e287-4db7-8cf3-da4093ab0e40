69addda4e59df3248e4cf6c3f5598725eab78ff9
rishikant3 <rishikant5sep@gmail.com>
2022-03-10 09:13:36 +0000

    hi

--- angularapp/src/app/plan-serv.service.ts
+++ angularapp/src/app/plan-serv.service.ts
@@ -1,16 +1,37 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Plan } from './plan';
 
-import { PlanServService } from './plan-serv.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanServService {
 
-describe('PlanServService', () => {
-  let service: PlanServService;
+  private baseURL = "https://8080-fcaafabafbacafecddebfdaffdacedbbebcbf.examlyiopb.examly.io/admin/addPlan";
+  private baseUrl1 = "https://8080-fcaafabafbacafecddebfdaffdacedbbebcbf.examlyiopb.examly.io/admin/viewPlan";
+  private baseUrl2 = "https://8080-fcaafabafbacafecddebfdaffdacedbbebcbf.examlyiopb.examly.io/admin/deletePlan";
+  private baseUrl3 = "https://8080-fcaafabafbacafecddebfdaffdacedbbebcbf.examlyiopb.examly.io/admin/updatePlan";
+  constructor(private httpClient: HttpClient) { }
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PlanServService);
-  });
+  createPlan(plan: Plan): Observable<Object>{
+    return this.httpClient.post(this.baseURL, plan);
+  }
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  getPlans(): Observable<Plan[]>{
+    return this.httpClient.get<Plan[]>(this.baseUrl1);
+  }
+
+  getPlanById(id: number): Observable<Plan>{
+    return this.httpClient.get<Plan>(`${this.baseUrl1}/${id}`);
+  }
+
+  updatePlan(id: number, plan: Plan): Observable<Object>{
+    return this.httpClient.put(`${this.baseUrl3}/${id}`, plan);
+  }
+
+  deletePlan(planId:number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseUrl2}/${planId}`);
+  }
+
+}
