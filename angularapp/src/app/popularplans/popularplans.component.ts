5047edd0da5c29e5fc2ff2d0e317bec04167fc6c
Joshika-jaya <joshikajehan@gmail.com>
2022-03-11 08:03:53 +0000

    joshika

--- angularapp/src/app/popularplans/popularplans.component.ts
+++ angularapp/src/app/popularplans/popularplans.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PlanServService } from '../plan-serv.service';
+import { Plan } from '../plan';
 
 @Component({
   selector: 'app-popularplans',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./popularplans.component.css']
 })
 export class PopularplansComponent implements OnInit {
-
-  constructor() { }
-
+  x:any;
+  plan: Plan = new Plan();
+  plans: Plan[] | undefined;
+  constructor(private planService: PlanServService, private router: Router) { }
+  private getPlan() {
+       this.planService.getPlans().subscribe(data => {
+        this.plans = data;
+        console.log(data);
+      });
+  }
   ngOnInit(): void {
+    this.getPlan();
   }
 
 }
